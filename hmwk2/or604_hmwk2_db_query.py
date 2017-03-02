80d69918d91914686176c9f35db1c5230872fd7b
Melissa Quijada <mquijada011@gmail.com>
2017-03-01 23:31:12 -0500

    work on NY McDonalds distance

--- hmwk2/or604_hmwk2_db_query.py
+++ hmwk2/or604_hmwk2_db_query.py
@@ -1,37 +1,56 @@
 from geopy.distance import vincenty
 import sqlite3
+import pandas
 
 def calculate_distance(lat1, lon1, lat2, lon2):
-    return vincenty((lat1, lon1), (lat2, lon2)).miles
+    point1 = (lat1, lon1)
+    point2 = (lat2, lon2)
+    return vincenty(point1, point2).miles
 
 
 conn = sqlite3.connect('database2.db')
 conn.create_function("calculate_distance", 4, calculate_distance)
 cursor = conn.cursor()
+
 cursor.execute('''
-SELECT A.storeNumber
-FROM (
-SELECT
-    Nearby.storeNumber AS storeNumber,
-    calculate_distance(Nearby.latitude, Nearby.longitude, NY.latitude, NY.longitude) AS distance
-FROM
-    McDonaldsLocations AS Nearby,
-    McDonaldsLocations AS NY
-WHERE
-    Nearby.state IN ('PA', 'NJ', 'CT', 'RI', 'MA', 'NH', 'VT', 'OH', 'MD', 'DE')
-        AND
-    NY.state = 'NY'
-) AS A
-GROUP BY A.storeNumber
-HAVING Max(A.distance) <= 100;
+ SELECT
+     Nearby.storeNumber AS storeNumber,
+     MAX(calculate_distance(Nearby.latitude, Nearby.longitude, NY.latitude, NY.longitude)) AS distance
+ FROM
+     McDonaldsLocations AS Nearby,
+     McDonaldsLocations AS NY
+ WHERE
+     Nearby.state IN ('PA', 'NJ', 'CT', 'RI', 'MA', 'NH', 'VT', 'OH', 'MD', 'DE', 'NY')
+         AND
+     NY.state = 'NY'
+GROUP BY Nearby.storeNumber
+
 ''')
 
-table = cursor.fetchall()
-print 'Store numbers:'
-for row in table:
-    print row
+results = cursor.fetchall()
+for row in results:
+    print row 
 
-#
+
+
+# SELECT A.storeNumber
+# FROM (
+# SELECT
+#     Nearby.storeNumber AS storeNumber,
+#     calculate_distance(Nearby.latitude, Nearby.longitude, NY.latitude, NY.longitude) AS distance
+# FROM
+#     McDonaldsLocations AS Nearby,
+#     McDonaldsLocations AS NY
+# WHERE
+#     Nearby.state IN ('PA', 'NJ', 'CT', 'RI', 'MA', 'NH', 'VT', 'OH', 'MD', 'DE')
+#         AND
+#     NY.state = 'NY'
+# ) AS A
+# GROUP BY A.storeNumber
+# HAVING Max(A.distance) <= 100;
+
+
+
 # for j in restaurants:
 #     selectedThreeNY = []
 #     if restaurant['addressRegion'] == "NY":
